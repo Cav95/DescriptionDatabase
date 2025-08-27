bf3b84c8cf7141aff66ecc7b3b005e32fc82ab66
Mattia Cavina <mattiacavina95@gmail.com>
2025-08-27 10:40:07 +0200

    Fix UpdateMEthod

--- src/main/java/descriptionupdate/view/scenes/UpdateDescriptionDialog.java
+++ src/main/java/descriptionupdate/view/scenes/UpdateDescriptionDialog.java
@@ -26,104 +26,98 @@ import java.awt.event.ActionListener;
  */
 public class UpdateDescriptionDialog extends JDialog {
 
-    private static final long serialVersionUID = 1L;
-    private static final String FONT = "Roboto";
+        private static final long serialVersionUID = 1L;
+        private static final String FONT = "Roboto";
 
-    private JLabel itaLabel = new JLabel("ITA Description:");
-    private JLabel engLabel = new JLabel("ENG Description:");
+        private JLabel itaLabel = new JLabel("ITA Description:");
+        private JLabel engLabel = new JLabel("ENG Description:");
 
-    private JTextField itaTextField = GuiFactory.getTextField(20);
-    private JTextField engTextField = GuiFactory.getTextField(20);
+        private JTextField itaTextField = GuiFactory.getTextField(20);
+        private JTextField engTextField = GuiFactory.getTextField(20);
 
-    private final JPanel mainPanel = new JPanel();
-    private final JPanel titlePannel = new JPanel();
-    private JPanel bottomPanel = new JPanel();
-    private JLabel titleLabel = new JLabel();
+        private final JPanel mainPanel = new JPanel();
+        private final JPanel titlePannel = new JPanel();
+        private JPanel bottomPanel = new JPanel();
+        private JLabel titleLabel = new JLabel();
 
-    /**
-     * Constructor for ResultPane.
-     * 
-     * @param view       the main view of the application
-     * @param title      the title of the dialog
-     * @param removeMode whether to remove the dialog after displaying results
-     * @param result     the result string to be displayed
-     */
-    public UpdateDescriptionDialog(final View view, final String title, final String exIta, final String exEng,
-            final String exGroup) {
+        /**
+         * Constructor for ResultPane.
+         * 
+         * @param view       the main view of the application
+         * @param title      the title of the dialog
+         * @param removeMode whether to remove the dialog after displaying results
+         * @param result     the result string to be displayed
+         */
+        public UpdateDescriptionDialog(final View view, final String title, final String exIta, final String exEng,
+                        final String exGroup) {
 
-        super(view.getMainFrame(), title, ModalityType.APPLICATION_MODAL);
+                super(view.getMainFrame(), title, ModalityType.APPLICATION_MODAL);
 
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setSize(800, 500);
-        this.setMaximumSize(this.getSize());
-        this.setLocationRelativeTo(view.getMainFrame());
-        this.setResizable(true);
+                this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                this.setSize(800, 500);
+                this.setMaximumSize(this.getSize());
+                this.setLocationRelativeTo(view.getMainFrame());
+                this.setResizable(true);
 
-        this.setLayout(new BorderLayout());
+                this.setLayout(new BorderLayout());
 
-        titlePannel.setLayout(new BorderLayout());
-        titlePannel.setBorder(BorderFactory.createEmptyBorder(50, 20, 20, 20));
-        this.add(titlePannel, BorderLayout.NORTH);
+                titlePannel.setLayout(new BorderLayout());
+                titlePannel.setBorder(BorderFactory.createEmptyBorder(50, 20, 20, 20));
+                this.add(titlePannel, BorderLayout.NORTH);
 
-        titleLabel = new JLabel(
-                "Stai aggiornando la descrizione: " + exGroup + " - " + exIta + " - " + exEng);
-        titleLabel.setFont(new Font(FONT, Font.BOLD, 15));
-        titleLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        titlePannel.add(titleLabel, BorderLayout.CENTER);
+                titleLabel = new JLabel(
+                                "Stai aggiornando la descrizione: " + exGroup + " - " + exIta + " - " + exEng);
+                titleLabel.setFont(new Font(FONT, Font.BOLD, 15));
+                titleLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+                titlePannel.add(titleLabel, BorderLayout.CENTER);
 
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(40, 20, 20, 20));
+                mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+                mainPanel.setBorder(BorderFactory.createEmptyBorder(40, 20, 20, 20));
 
-        itaTextField.setText(exIta);
-        engTextField.setText(exEng);
+                itaTextField.setText(exIta);
+                engTextField.setText(exEng);
 
-        mainPanel.add(itaLabel);
-        mainPanel.add(itaTextField);
+                mainPanel.add(itaLabel);
+                mainPanel.add(itaTextField);
 
-        mainPanel.add(engLabel);
-        mainPanel.add(engTextField);
+                mainPanel.add(engLabel);
+                mainPanel.add(engTextField);
 
-        this.add(mainPanel, BorderLayout.CENTER);
+                this.add(mainPanel, BorderLayout.CENTER);
 
-        // Correggi il nome del pannello
+                // Correggi il nome del pannello
 
-        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
-        mainPanel.add(bottomPanel, BorderLayout.EAST);
+                bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.Y_AXIS));
+                mainPanel.add(bottomPanel, BorderLayout.EAST);
 
-        // Pulsante "Stampa"
-        JButton update = GuiFactory.getButtom("Aggiorna", Color.GRAY, Color.BLACK, Font.getFont(FONT),
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        try {
-                            view.getController().updateDescription(
-                                    new Description(exIta, exEng, exGroup),
-                                    new Description(itaTextField.getText()
-                                            .toUpperCase(),
-                                            engTextField.getText()
-                                                    .toUpperCase(),
-                                            exGroup));
-                            JOptionPane.showMessageDialog(UpdateDescriptionDialog.this,
-                                    "Description updated successfully!\n"
-                                            + itaTextField.getText()
-                                                    .toUpperCase()
-                                            + " - "
-                                            + engTextField.getText()
-                                                    .toUpperCase()
-                                            + " - "
-                                            + exGroup);
-                            view.getController().setSaved(false); // Mark as not saved
-                            view.getController().initialScene();
-                            UpdateDescriptionDialog.this.dispose();
+                // Pulsante "Stampa"
+                JButton update = GuiFactory.getButtom("Aggiorna", Color.GRAY, Color.BLACK, Font.getFont(FONT),
+                                new ActionListener() {
+                                        @Override
+                                        public void actionPerformed(final ActionEvent e) {
+                                                try {
+                                                        var itaDes = itaTextField.getText().toUpperCase();
+                                                        var engDes = engTextField.getText().toUpperCase();
+                                                        view.getController().updateDescription(
+                                                                        new Description(exIta, exEng, exGroup),
+                                                                        new Description(itaDes, engDes, exGroup));
+                                                        JOptionPane.showMessageDialog(UpdateDescriptionDialog.this,
+                                                                        "Description updated successfully!\n"
+                                                                                        + itaDes + " - "
+                                                                                        + engDes + " - "
+                                                                                        + exGroup);
+                                                        view.getController().setSaved(false); // Mark as not saved
+                                                        view.goToInitialScene();
+                                                        UpdateDescriptionDialog.this.dispose();
 
-                        } catch (Exception ex) {
-                            JOptionPane.showMessageDialog(UpdateDescriptionDialog.this,
-                                    "Errore " + ex.getMessage());
-                        }
-                    }
+                                                } catch (Exception ex) {
+                                                        JOptionPane.showMessageDialog(UpdateDescriptionDialog.this,
+                                                                        "Errore " + ex.getMessage());
+                                                }
+                                        }
 
-                });
-        bottomPanel.add(update);
-    }
+                                });
+                bottomPanel.add(update);
+        }
 
 }
