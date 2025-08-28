67f631160a9dbbd6446983beb842dbea012ea7a8
Mattia Cavina <mattiacavina95@gmail.com>
2025-08-28 08:22:52 +0200

    Add controllo to proben caracter

---
+++ src/main/java/descriptionupdate/model/api/ProibenCaratter.java
@@ -0,0 +1,35 @@
+package descriptionupdate.model.api;
+
+/**
+ * Enum representing various prohibited characters.
+ */
+public enum ProibenCaratter {
+
+    PARENTHESES_OPEN("("),
+    PARENTHESES_CLOSE(")"),
+    AMPERSAND("&"),
+    PERCENT("%"),
+    COMMA(","),
+    QUESTION_MARK("?"),
+    PIPE("|"),
+    EXCLAMATION_MARK("!"),
+    AT("@"),
+    SQUARE_BRACKET_OPEN("["),
+    SQUARE_BRACKET_CLOSE("]"),
+    DOLLAR("$"),
+    VIRGOLETTE("\"");
+
+    private String character;
+
+    ProibenCaratter(String character) {
+        this.character = character;
+    }
+
+    /**
+     * Gets the character representation of the enum constant.
+     * @return The character as a string.
+     */
+    public String getCharacter() {
+        return character;
+    }
+}
